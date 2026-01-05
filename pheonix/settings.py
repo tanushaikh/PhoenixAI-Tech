76517b3db7015a1024da48ccd5adad39347639f7
tanushaikh <tanushaikh00786@gmail.com>
2026-01-05 23:20:06 +0530

    vercel.py final fix

--- pheonix/settings.py
+++ pheonix/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'blogs/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
--- pheonixAI/urls.py
+++ pheonixAI/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index,about,contact,services,projects,blogs,get_blog_details
 urlpatterns = [
     path('', index, name='index'),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('blogs', blogs, name='blogs'),
     path('blog/<int:id>/', get_blog_details, name='blog'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
